19586ea69ff1e99cf189a177d960aebd38464e00
Jorge Balazs <jabalazst@gmail.com>
2017-06-21 22:02:33 +0900

    Chang parser for extracting by subparser

--- pdfly.py
+++ pdfly.py
@@ -4,26 +4,43 @@ import argparse
 from pdfly import (pdf_utils, type_validation)
 import sys
 
-parser = argparse.ArgumentParser(description='Performs basic operations on PDF files')
+parser = argparse.ArgumentParser(description='Performs basic operations on '
+                                             'PDF files')
+
+subparsers = parser.add_subparsers()
+extract_subparser = subparsers.add_parser(
+                               "extract",
+                               help='Extract pages from a PDF file',
+                               description='Extract pages from a PDF '
+                                           'file and save them to a new file')
+extract_subparser.add_argument('infile',
+                               type=str,
+                               help='Input PDF to be processed')
+extract_subparser.add_argument('outfile',
+                               type=str,
+                               help='Path of the output file')
+extract_subparser.add_argument('from_page', type=int,
+                               help="""First page to be extracted. Both 0 and 1
+                                    mean the first page""")
+extract_subparser.add_argument('to_page', type=int,
+                               help="""Last page to be extracted. 0 means the
+                                    last one""")
+extract_subparser.set_defaults(func='extract')
+
+merge_parser = subparsers.add_parser("merge", help='Tools for merging PDFs or part of them')
+
 parser.add_argument('-v', '--verbose', help='Increase output verbosity',
                     action='store_true')
-parser.add_argument('infile', type=str, help='Input PDF to be processed')
-parser.add_argument('outfile', type=str, help='Path of the output file')
-parser.add_argument('-e',
-                    '--extract',
-                    type=int,
-                    help="""Extracts the selected range of pages from the input
-                    PDF and saves it to the specified output. beginning_page equals 0
-                    is equivalent to the first page; end_page equals 0 is equivalent
-                    to the last page""",
-                    nargs=2,
-                    metavar=('from_page', 'to_page'))
 
 args =  parser.parse_args()
+print(args)
 
-if args.extract is not None:
-    from_page = args.extract[0]
-    to_page = args.extract[1]
+
+if args.func == 'extract':
+    from_page = args.from_page
+    to_page = args.to_page
     type_validation.validate_extract_pages(from_page, to_page)
     pdf_utils.extract(args.infile, args.outfile, from_page, to_page)
-    sys.exit(0)
+
+elif args.merge:
+    pass
