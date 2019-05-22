5cde9f051d6f71b782c7f338d55cd3a26b423573
Jorge Andrés Balazs <jabalazst@gmail.com>
2019-05-22 12:48:59 +0900

    Blackify code, remove unused main

--- pdfly.py
+++ pdfly.py
@@ -1,52 +1,67 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 from __future__ import print_function
 import argparse
-from pdfly import (pdf_utils, type_validation)
+from pdfly import pdf_utils, type_validation
 import sys
 
-parser = argparse.ArgumentParser(description='Performs basic operations on '
-                                             'PDF files')
+parser = argparse.ArgumentParser(
+    description="Performs basic operations on " "PDF files"
+)
 
 subparsers = parser.add_subparsers()
 extract_subparser = subparsers.add_parser(
-                               "extract",
-                               help='Extract pages from a PDF file',
-                               description='Extract pages from a PDF '
-                                           'file and save them to a new file')
-extract_subparser.add_argument('infile',
-                               type=str,
-                               help='Input PDF to be processed')
-extract_subparser.add_argument('outfile',
-                               type=str,
-                               help='Path of the output file')
-extract_subparser.add_argument('from_page', type=int,
-                               help="""First page to be extracted. Both 0 and 1
-                                    mean the first page""")
-extract_subparser.add_argument('to_page', type=int,
-                               help="""Last page to be extracted. 0 means the
-                                    last one""")
-extract_subparser.set_defaults(func='extract')
+    "extract",
+    help="Extract pages from a PDF file",
+    description="Extract pages from a PDF " "file and save them to a new file",
+)
+extract_subparser.add_argument(
+    "infile", type=str, help="Input PDF to be processed"
+)
+extract_subparser.add_argument(
+    "outfile", type=str, help="Path of the output file"
+)
+extract_subparser.add_argument(
+    "from_page",
+    type=int,
+    help="""First page to be extracted. Both 0 and 1
+                                    mean the first page""",
+)
+extract_subparser.add_argument(
+    "to_page",
+    type=int,
+    help="""Last page to be extracted. 0 means the
+                                    last one""",
+)
+extract_subparser.set_defaults(func="extract")
 
 merge_subparser = subparsers.add_parser(
-                                  "merge",
-                                  help='Tools for merging PDFs or part of them')
-merge_subparser.add_argument("-o", "--outfile", type=str, help='Path of the output PDF')
-merge_subparser.add_argument("pdfs_to_merge", type=str, nargs='+',
-                          help="""Paths to the PDF files to be merged""")
-merge_subparser.set_defaults(func='merge')
+    "merge", help="Tools for merging PDFs or part of them"
+)
+merge_subparser.add_argument(
+    "-o", "--outfile", type=str, help="Path of the output PDF"
+)
+merge_subparser.add_argument(
+    "pdfs_to_merge",
+    type=str,
+    nargs="+",
+    help="""Paths to the PDF files to be merged""",
+)
+merge_subparser.set_defaults(func="merge")
 
-parser.add_argument('-v', '--verbose', help='Increase output verbosity',
-                    action='store_true')
+parser.add_argument(
+    "-v", "--verbose", help="Increase output verbosity", action="store_true"
+)
 
-args =  parser.parse_args()
+args = parser.parse_args()
 print(args)
 
 
-if args.func == 'extract':
+if args.func == "extract":
     from_page = args.from_page
     to_page = args.to_page
     type_validation.validate_extract_pages(from_page, to_page)
     pdf_utils.extract(args.infile, args.outfile, from_page, to_page)
 
-elif args.func == 'merge':
+elif args.func == "merge":
     pdf_utils.merge(args.outfile, args.pdfs_to_merge)
--- pdfly/pdf_utils.py
+++ pdfly/pdf_utils.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python
-from __future__ import print_function
 from PyPDF2 import PdfFileWriter, PdfFileReader
+
 
 def extract(document, new_document, from_page=0, to_page=0):
     """
@@ -26,69 +25,18 @@ def extract(document, new_document, from_page=0, to_page=0):
     output.write(outputStream)
     return 0
 
+
 def _add_full_pdf_to_writer(pdf_writer, pdf_reader):
     for i in range(pdf_reader.getNumPages()):
         page = pdf_reader.getPage(i)
         pdf_writer.addPage(page)
     return pdf_writer
 
+
 def merge(outfile, file_list):
     pdf_writer = PdfFileWriter()
     for filepath in file_list:
         pdf_file = PdfFileReader(open(filepath, "rb"))
         pdf_writer = _add_full_pdf_to_writer(pdf_writer, pdf_file)
-    output_stream = file(outfile, 'wb')
+    output_stream = file(outfile, "wb")
     pdf_writer.write(output_stream)
-
-
-def main():
-    output = PdfFileWriter()
-    input1 = PdfFileReader(open("document1.pdf", "rb"))
-
-    # print how many pages input1 has:
-    print("document1.pdf has %d pages." % input1.getNumPages())
-
-    # add page 1 from input1 to output document, unchanged
-    output.addPage(input1.getPage(0))
-
-    # add page 2 from input1, but rotated clockwise 90 degrees
-    output.addPage(input1.getPage(1).rotateClockwise(90))
-
-    # add page 3 from input1, rotated the other way:
-    output.addPage(input1.getPage(2).rotateCounterClockwise(90))
-    # alt: output.addPage(input1.getPage(2).rotateClockwise(270))
-
-    # add page 4 from input1, but first add a watermark from another PDF:
-    page4 = input1.getPage(3)
-    watermark = PdfFileReader(open("watermark.pdf", "rb"))
-    page4.mergePage(watermark.getPage(0))
-    output.addPage(page4)
-
-
-    # add page 5 from input1, but crop it to half size:
-    page5 = input1.getPage(4)
-    page5.mediaBox.upperRight = (
-        page5.mediaBox.getUpperRight_x() / 2,
-        page5.mediaBox.getUpperRight_y() / 2
-    )
-    output.addPage(page5)
-
-    # add some Javascript to launch the print window on opening this PDF.
-    # the password dialog may prevent the print dialog from being shown,
-    # comment the the encription lines, if that's the case, to try this out
-    output.addJS("this.print({bUI:true,bSilent:false,bShrinkToFit:true});")
-
-    # encrypt your new PDF and add a password
-    password = "secret"
-    output.encrypt(password)
-
-    # finally, write "output" to document-output.pdf
-    outputStream = file("PyPDF2-output.pdf", "wb")
-    output.write(outputStream)
-
-if __name__ == '__main__':
-    # main()
-    extract("document1.pdf", "splitted.pdf", to_page=5)
-    # print(pages_are_valid(3, 3))
-    # print(pages_are_valid(4, 3))
-    # print(pages_are_valid(3, 'a'))
